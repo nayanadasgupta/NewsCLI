68da903a59eb7322ffc01b2ec134e75cf83d6212
nayanadasgupta <nayana.dasgupta@outlook.com>
2020-06-07 18:26:01 +0100

    Added books and fixed ordering of commands

--- news.py
+++ news.py
@@ -1,24 +1,46 @@
 import click
 import requests
+from collections import OrderedDict
 from pyfiglet import figlet_format
 
 api_key = "7Rli8AUFCxc0wmSEf8CxAp9AUp86Iejh"
 
-@click.group()
+
+class NaturalOrderGroup(click.Group):
+    """
+    Orders commands in help page in the order they were added instead of alphabetically.
+    """
+
+    def __init__(self, name=None, commands=None, **attrs):
+        super(NaturalOrderGroup, self).__init__(
+            name=name, commands=None, **attrs)
+        if commands is None:
+            commands = OrderedDict()
+        elif not isinstance(commands, OrderedDict):
+            commands = OrderedDict(commands)
+        self.commands = commands
+
+    def list_commands(self, ctx):
+        return self.commands.keys()
+
+
+@click.group(cls=NaturalOrderGroup)
 def cli():
     """
-    Simple CLI for querying the top New York Times stories. Data provided by The New York Times.
+    Simple CLI for querying New York Times articles. Data provided by The New York Times.
     Find out more by entering news COMMAND --help.
     """
     click.echo(figlet_format("News CLI"))
     click.echo("Data provided by The New York Times.\n")
     pass
 
+
 @click.command()
-@click.option("--count", default=1, help="Number of top stories.",type=int)
-@click.option('--type', default='main', help="Type of top stories. Options are main, arts, business, health, politics, science," +
-                                             "tech and world. Default is main.")
-def topStories(count,type):
+@click.option("--count", default=1, help="Number of top stories.", type=int)
+@click.option('--type', default='main',
+              help="Type of top stories. Options are main, arts, business, health, politics, science," +
+                   "tech and world. Default is main.")
+def topstories(count, type):
     """
     Top Stories.
     """
@@ -59,8 +81,9 @@ def topStories(count,type):
         except IndexError:
             click.echo("No remaining news stories in this category\n")
 
+
 @click.command()
-@click.option("--count", default=1, help="Number of popular stories.",type=int)
+@click.option("--count", default=1, help="Number of popular stories.", type=int)
 def mostviewed(count):
     """
         Most viewed articles in the last seven days.
@@ -79,8 +102,9 @@ def mostviewed(count):
         except IndexError:
             click.echo("No remaining news stories in this category\n")
 
+
 @click.command()
-@click.option("--count", default=1, help="Number of popular stories.",type=int)
+@click.option("--count", default=1, help="Number of popular stories.", type=int)
 def mostrecent(count):
     """
         Most recent articles published.
@@ -99,12 +123,13 @@ def mostrecent(count):
         except IndexError:
             click.echo("No remaining news stories in this category\n")
 
+
 @click.command()
 @click.argument("keyword")
-@click.option("--count", default=1, help="Number of stories.",type=int)
-def search(keyword,count):
+@click.option("--count", default=1, help="Number of stories.", type=int)
+def search(keyword, count):
     """
-    Search articles by a keyword
+    Search articles by a keyword.
     """
     url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&api-key=" + api_key
     response = requests.get(url)
@@ -120,8 +145,34 @@ def search(keyword,count):
         except IndexError:
             click.echo("No remaining news stories in this category\n")
 
-cli.add_command(topStories)
+
+@click.command()
+@click.option("--type", default="fiction", help="Types of books. Options are fiction, nonfiction. Default is fiction.")
+@click.option("--count", default=1, help="Number of stories.", type=int)
+def books(type, count):
+    """
+    Find bestselling books.
+    """
+    if type == 'nonfiction':
+        url = "https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=" + api_key
+    else:
+        url = "https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=" + api_key
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        click.echo("An error has occurred - please wait 30 seconds before trying again.")
+    else:
+        try:
+            for i in range(count):
+                click.echo(response.json()['results']['books'][i]['title'])
+                click.echo(response.json()['results']['books'][i]['author'])
+                click.echo(response.json()['results']['books'][i]['description'])
+                click.echo(response.json()['results']['books'][i]['amazon_product_url'] + "\n")
+        except IndexError:
+            click.echo("No remaining books in this list\n")
+
+
+cli.add_command(topstories)
+cli.add_command(search)
 cli.add_command(mostviewed)
 cli.add_command(mostrecent)
-cli.add_command(search)
-
+cli.add_command(books)
